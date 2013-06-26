3dd4653c59f001dd68e81c31690c3591e150b7f2
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-06-26 12:22:13 -0400

    extracted the prompt rewriting parts

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,4 @@
-﻿#theme
-$cdelim = [ConsoleColor]::DarkCyan;
-$chost = [ConsoleColor]::Green;
-$cloc = [ConsoleColor]::Cyan;
-$cerr = [ConsoleColor]::Red;
-
-$isWin7AndUp = [Environment]::OSVersion.Version -ge (new-object 'Version' 6,1)
+﻿$isWin7AndUp = [Environment]::OSVersion.Version -ge (new-object 'Version' 6,1)
 
 # Add necessary registry entry to be able to use the TFPT cmdlets from x64
 if ($isWin7AndUp) {
@@ -24,28 +18,10 @@ if ((Test-Path $tfptPath) -eq $True) {
 
 Import-Module tfdash
 
-# Initialize Visual Studio command line environment
 Initialize-VsVars32 > $null
 
-# Overriding the command prompt
 function prompt
 {
-	Write-Host "$([char]0x0A7) " -n -f $cloc
-	Write-Host "{" -n -f $cdelim
-	Write-Host (Get-ShortenedPath (pwd).Path) -n -f $cloc
-	Write-Host "}" -n -f $cdelim
-	
-    # In Tfs.psm1
-    # TODO: Fix defect that causes tf.exe to be called regardless if we're in a workspace or not (SLOW)
-	Set-TfsGlobals
-
-	if ($global:currentTfsBranch -ne $null) {
-		Write-Host " [" -n -f $cdelim
-		Write-Host $global:currentTfsBranch -n -f $chost
-		Write-Host " " -n
-		Write-Host $global:localVersion -n -f DarkYellow
-		Write-Host "]" -n -f $cdelim
-	}
-		
-    return " "
+  Write-VcsStatus
+  return '> '
 }
---
+++ lib/Write-VcsStatus.ps1
@@ -0,0 +1,24 @@
+#theme
+$cdelim = [ConsoleColor]::DarkCyan;
+$chost = [ConsoleColor]::Green;
+$cloc = [ConsoleColor]::Cyan;
+$cerr = [ConsoleColor]::Red;
+
+function Write-VcsStatus 
+{
+  Write-Host "$([char]0x0A7) " -n -f $cloc
+	Write-Host "{" -n -f $cdelim
+	Write-Host (Get-ShortenedPath (pwd).Path) -n -f $cloc
+	Write-Host "}" -n -f $cdelim
+	
+  # TODO: Fix defect that causes tf.exe to be called regardless if we're in a workspace or not (SLOW)
+	Set-TfsGlobals
+
+	if ($global:currentTfsBranch -ne $null) {
+		Write-Host " [" -n -f $cdelim
+		Write-Host $global:currentTfsBranch -n -f $chost
+		Write-Host " " -n
+		Write-Host $global:localVersion -n -f DarkYellow
+		Write-Host "]" -n -f $cdelim
+	}
+}
