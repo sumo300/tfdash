29061c41453339d6ea7d07a9bd0f893731ce4cff
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-06-26 12:40:42 -0400

    moved the suspicious tfpt cmdlet/reg stuff into a separate script file

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,22 +1,4 @@
-﻿$isWin7AndUp = [Environment]::OSVersion.Version -ge (new-object 'Version' 6,1)
-
-# Add necessary registry entry to be able to use the TFPT cmdlets from x64
-if ($isWin7AndUp) {
-	if (-not (Test-Path registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PowerShell\1\PowerShellSnapIns\Microsoft.TeamFoundation.PowerShell)) {
-		$regPath = Join-Path $localModulesPath -ChildPath "tfpt-x64.reg"
-		regedit /s "$regPath"
-	}
-}
-
-# Make the TFPT PowerShell Cmdlets available
-$tfptPath = Join-Path $programFiles -ChildPath "Microsoft Team Foundation Server 2010 Power Tools\Microsoft.TeamFoundation.PowerTools.PowerShell.dll"
-if ((Test-Path $tfptPath) -eq $True) {
-    Add-PSSnapin Microsoft.TeamFoundation.PowerShell
-} else {
-    Write-Warning "Microsoft Team Foundation Server 2010 Power Tools PowerShell CmdLets failed to register.  Make sure the cmdlets are installed (from TFPT installer).  If they are, run your PowerShell command line again if this was your first time using them."
-}
-
-Import-Module tfdash
+﻿Import-Module tfdash
 
 Initialize-VsVars32 > $null
 
