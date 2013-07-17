c0d74ea8563e7cd2cfa56ef6dea33bd1545513a6
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-07-17 12:28:30 -0400

    renamed Invoke-TfsUndoUnchanged to Undo-TfsUnchanged, fixes #12

--- lib/Undo-TfsUnchanged.ps1
+++ lib/Undo-TfsUnchanged.ps1
@@ -1,11 +1,11 @@
-function Invoke-TfsUndoUnchanged {
+function Undo-TfsUnchanged {
 <#
 	.SYNOPSIS
 	Undo unchanged files
 	.DESCRIPTION
 	Uses the tfpt uu command to undo any unchanged files recursively when compared to the latest changes.
 	.EXAMPLE
-	Invoke-TfsUndoUnchanged
+	Undo-TfsUnchanged
 #>
 	Write-Host " "
 	Write-Host "Undoing unchanged files recursively..." -f $cloc
