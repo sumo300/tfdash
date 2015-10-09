5e4a57af8208ed057370fdc22a6258d05e107390
Michael Sumerano <5069447+sumo300@users.noreply.github.com>
2015-10-09 03:08:21 -0400

    Updated Register-TfptCmdLets to require PowerShell v3 and up and attempting to register TFPT 2015 under x64

--- lib/Register-TfptCmdlets.ps1
+++ lib/Register-TfptCmdlets.ps1
@@ -1,31 +1,32 @@
-$tfpt_reg_key = 'HKLM:\SOFTWARE\Microsoft\PowerShell\1\PowerShellSnapIns\Microsoft.TeamFoundation.PowerShell'
-$tfpt_reg_key_psv3 = 'HKLM:\SOFTWARE\Microsoft\PowerShell\3\PowerShellSnapIns\Microsoft.TeamFoundation.PowerShell'
-$isWin7AndUp = [Environment]::OSVersion.Version -ge (New-Object 'Version' 6,1)
-
-# Add necessary registry entry to be able to use the TFPT cmdlets from x64
-function Register-TfptCmdlets64([string]$version = "2015") {
-  if ($isWin7AndUp) {
-    if (-not (Test-Path $tfpt_reg_key)) {
-      regedit /s $PSScriptRoot\tfpt$version-x64.reg
-    }
-  }
-}
-
-# Add necessary registry entry to be able to use the TFPT cmdlets from x64 and PowerShell v3
-#function Register-TfptCmdlets64ForPoshV3 {
-#  if ($isWin7AndUp) {
-#    if (-not (Test-Path $tfpt_reg_key_psv3)) {
-#      regedit /s $PSScriptRoot\tfpt-x64-psv3.reg
-#    }
-#  }
-#}
-
 # Make the TFPT PowerShell Cmdlets available
 function Register-TfptCmdlets([string]$version = "2015") {
+	$posh_reg_key = 'HKLM:\SOFTWARE\Microsoft\PowerShell\'
+	$tfpt_suffix = '\PowerShellSnapIns\Microsoft.TeamFoundation.PowerShell'
+	$poshVersion = $PSVersionTable.PSVersion.Major
+	
+	if ($poshVersion -lt 3) {
+		Write-Warning "PowerShell v3 or later is required to register Microsoft Team Foundation Server $version Power Tools PowerShell CmdLets."
+		return
+	}
+	
+	if (Test-Win64) {
+		$psv = Join-Path $posh_reg_key $poshVersion
+		$tfpt_reg_key = Join-Path $psv $tfpt_suffix
+			
+		if (-not (Test-Path $tfpt_reg_key)) {
+			$regPath = Join-Path $PSScriptRoot "..\tfpt$version-x64.reg"
+			regedit /s $regPath
+		}
+	}
+	
 	$tfpt_assm = "${Env:ProgramFiles(x86)}\Microsoft Team Foundation Server $version Power Tools\Microsoft.TeamFoundation.PowerTools.PowerShell.dll"
 	if (Test-Path $tfpt_assm) {
 		Add-PSSnapin Microsoft.TeamFoundation.PowerShell
 	} else {
 		Write-Warning "Microsoft Team Foundation Server $version Power Tools PowerShell CmdLets failed to register.  Make sure the cmdlets are installed (from TFPT installer).  If they are, run your PowerShell command line again if this was your first time using them."
 	}
+}
+
+function Test-Win64() {
+    return [IntPtr]::size -eq 8
 }
