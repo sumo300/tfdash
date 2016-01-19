f0bd37c6df1281999a92c56b2b2f4b3512bfc095
Michael Sumerano <5069447+sumo300@users.noreply.github.com>
2016-01-19 02:42:57 -0500

    Updated Invoke-TfsClean to use 'tf reconcile /clean' instead of relying on TFPT

--- lib/Invoke-TfsClean.ps1
+++ lib/Invoke-TfsClean.ps1
@@ -3,32 +3,32 @@ function Invoke-TfsClean([string]$params = "") {
 	.SYNOPSIS
 	Recursively deletes files and folders not under version control.
 	.DESCRIPTION
-	The Invoke-TfsClean function uses the TFS Power Tools "tfpt treeclean" command to 
+	The Invoke-TfsClean function uses the TFS "tf reconcile" command to 
 	recursively delete files and folders not under version control.
 	
 	This command is basically a clean-up to remove untracked files/folders.
 	.PARAMETER params
-	Allows additional tfpt treeclean switches to be specified.  Available 
+	Allows additional tf reconcile switches to be specified.  Available 
 	parameters:
 	
-	/noprompt              Operate in command-line mode only
-	/exclude:filespec[,..] Files and directories matching a filespec in this 
-						   list are excluded from processing
-	/preview               Do not make changes; only list the potential actions
-	/batchsize:num         Set the batch size for server calls (default 500)
-	filespec...            Only files and directories matching these filespecs
-						   are processed (inclusion list)
+	/noprompt                        Operate in command-line mode only
+	/preview                         Do not make changes; only list the potential actions
+    /noignore
+    /exclude:itemspec1,itemspec2,... Files and directories matching an itemspec
+                                     in this list are excluded from processing
+	itemspec                         Only files and directories matching these filespecs
+						             are processed (inclusion list)
 	.EXAMPLE
 	Invoke-TfsClean
 	.EXAMPLE
 	Invoke-TfsClean /noprompt
 #>
 	if ($params.Contains("/noprompt")) {
-		& $tfpt treeclean . /recursive $params | foreach-object {
+        & tf reconcile /clean /diff /recursive /unmapped . $params | foreach-object {
 			$item = "> " + $_
 			Write-Progress -id 1 -Activity "Cleaning up your work folder from the current directory" -status $item
 		}
 	} else {
-		& $tfpt treeclean . /recursive $params
+		& tf reconcile /clean /diff /recursive /unmapped . $params
 	}
 }
